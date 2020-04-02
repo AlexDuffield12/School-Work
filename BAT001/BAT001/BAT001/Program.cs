000c9e9d9bc4ac239262e91d8515bfbfd779246d
Alex <alex.duffield12@gmail.com>
2020-04-02 14:16:37 +0100

    BAT001 Halfway

---
+++ BAT001/BAT001/BAT001/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAT001
+{
+    class Program
+    {
+        //declaring the grid
+        static char[,] displaygrid;
+        static char[,] enemygrid;
+        public static char[,] CreateEmptyGrid(char[,] grid, int gridSize)
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; i++)
+                {
+                    grid[i, j] = '.';
+                }
+            }
+            return grid;
+        }
+
+        public static void DisplayGrid(char[,] grid, int gridSize)
+        {
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    Console.Write(grid[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            displaygrid = new char[8, 8];
+            enemygrid = new char[8, 8];
+
+            CreateEmptyGrid(displaygrid, 8);
+            CreateEmptyGrid(enemygrid, 8);
+
+            Console.ReadLine();
+        }
+    }
+}
