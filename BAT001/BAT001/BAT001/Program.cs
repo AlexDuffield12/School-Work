3551f87041ba4c6751a555bea7331826fbbb5a60
Alex <alex.duffield12@gmail.com>
2020-04-02 15:11:44 +0100

    BAT001 Complete

--- BAT001/BAT001/BAT001/Program.cs
+++ BAT001/BAT001/BAT001/Program.cs
@@ -15,7 +15,7 @@ namespace BAT001
         {
             for (int i = 0; i < gridSize; i++)
             {
-                for (int j = 0; j < gridSize; i++)
+                for (int j = 0; j < gridSize; j++)
                 {
                     grid[i, j] = '.';
                 }
@@ -35,6 +35,77 @@ namespace BAT001
             }
         }
 
+        public static void SetShips(char[,] grid)
+        {
+            grid[0, 0] = 'C';
+            grid[0, 1] = 'C';
+            grid[0, 2] = 'C';
+            grid[0, 3] = 'C';
+            grid[0, 4] = 'C';
+
+            grid[7, 0] = 'B';
+            grid[7, 1] = 'B';
+            grid[7, 2] = 'B';
+            grid[7, 3] = 'B';
+
+            grid[5, 7] = 'S';
+            grid[6, 7] = 'S';
+            grid[7, 7] = 'S';
+        }
+
+        static void CheckHit(char row, char col, char[,] display, char[,] enemy)
+        {
+            int x, y;
+            y = 0;
+
+            switch (row)
+            {
+                case 'A':
+                    y = 0;
+                    break;
+                case 'B':
+                    y = 1;
+                    break;
+                case 'C':
+                    y = 2;
+                    break;
+                case 'D':
+                    y = 3;
+                    break;
+                case 'E':
+                    y = 4;
+                    break;
+                case 'F':
+                    y = 5;
+                    break;
+                case 'G':
+                    y = 6;
+                    break;
+                case 'H':
+                    y = 7;
+                    break;
+
+            }
+
+
+            x = Int32.Parse(col.ToString()) - 1;
+
+            if (enemy[y, x] != '.')
+            {
+                Console.WriteLine("A hit!");
+                enemy[y, x] = 'X';
+                display[y, x] = 'X';
+                DisplayGrid(displaygrid, 8);
+            }
+            else
+            {
+                Console.WriteLine("A miss!");
+                enemy[y, x] = 'O';
+                display[y, x] = 'O';
+                DisplayGrid(displaygrid, 8);
+            }
+        }
+
         static void Main(string[] args)
         {
             displaygrid = new char[8, 8];
@@ -42,6 +113,18 @@ namespace BAT001
 
             CreateEmptyGrid(displaygrid, 8);
             CreateEmptyGrid(enemygrid, 8);
+            DisplayGrid(displaygrid, 8);
+            SetShips(enemygrid);
+            //DisplayGrid(enemygrid, 8);
+
+            while (true)
+            {
+                Console.Write("Enter A Grid reference in the format 'A3'");
+                string input = Console.ReadLine();
+                char row = input[0];
+                char col = input[1];
+                CheckHit(row, col, displaygrid, enemygrid);
+            }
 
             Console.ReadLine();
         }
