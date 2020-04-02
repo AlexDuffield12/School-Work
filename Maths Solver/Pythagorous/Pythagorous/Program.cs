5fd97e1e1839590627d79ef2562675e2984ebdb2
Alex <alex.duffield12@gmail.com>
2020-04-02 16:17:27 +0100

    Pythagorous solver

---
+++ Maths Solver/Pythagorous/Pythagorous/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsharpensGists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, p, b, sqh;
+
+            Console.WriteLine("Please Enter the Value of Perpendicular");
+            p = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please Enter the Value of Base");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            sqh = p * p + b * b;
+
+            h = Math.Sqrt(sqh);
+
+            Console.WriteLine("Hypotenius is {0}", h);
+            Console.ReadLine();
+        }
+    }
+}
